8d138572e67bcb5d9841ac242e5a7be7c36de0e7
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-11-17 17:33:16 +0200

    Implement Import

--- CSVLib/Sheet.cs
+++ CSVLib/Sheet.cs
@@ -19,7 +19,39 @@ namespace SheetLib
 
         public void Import(string fileName, SheetType type)
         {
-            throw new NotImplementedException();
+            this.Clear();
+
+            if (type == SheetType.CSV)
+            {
+                using (var reader = new StreamReader(fileName))
+                {
+                    var fileContent = reader.ReadToEnd();
+                    var rows = fileContent.Split(
+                        new string[] { "\r\n", "\n" },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    for (int i = 0; i < rows.Length; i++)
+                    {
+                        var rowRaw = rows[i];
+                        var columns = rowRaw.Split(
+                            new string[] { "," },
+                            StringSplitOptions.RemoveEmptyEntries);
+
+                        var row = new SheetRow();
+                        Rows.Add(row);
+
+                        for (int j = 0; j < columns.Length; j++)
+                        {
+                            var columnRaw = columns[j]; // cell value
+                            var column = new RowColumn(columnRaw);
+
+                            // Console.WriteLine($"Cell: {columnRaw}");
+                            row.Columns.Add(column);
+                        }
+                    }
+                }
+            }
+            else throw new NotImplementedException();
         }
 
         /// <summary>
