875d352f5905b9dd72c89b3277784064b7a30cb1
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-11-17 17:00:39 +0200

    Initial logic stuff

--- InCamp.Employees/Program.cs
+++ InCamp.Employees/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CSVLib;
+using System;
+using System.Diagnostics;
 
 namespace InCamp.Employees
 {
@@ -6,7 +8,27 @@ namespace InCamp.Employees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Input filename: ");
+            var inputFile = Console.ReadLine();
+
+            Console.Write("Output filename: ");
+            var outputFile = Console.ReadLine();
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Console.WriteLine("Reading file...");
+            var inputSheet = new CSV(inputFile);
+
+            Console.WriteLine("Generating a new one...");
+            var csv = new CSV();
+
+            // TODO: actually generate stuff
+
+            csv.Export(outputFile);
+
+            sw.Stop();
+            Console.WriteLine("Done in " + sw.Elapsed); 
         }
     }
 }
