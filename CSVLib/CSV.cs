5667a95227814184300cad924ddb63e4dae6beda
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-11-17 16:59:24 +0200

    Create and reference stub CSV library

---
+++ CSVLib/CSV.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSVLib
+{
+    public class CSV
+    {
+        public void Export(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Import(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Create an empty sheet
+        /// </summary>
+        public CSV() { }
+
+        /// <summary>
+        /// Import a sheet
+        /// </summary>
+        /// <param name="fileName">CSV file</param>
+        public CSV(string fileName) : this()
+        {
+            Import(fileName);
+        }
+    }
+}
