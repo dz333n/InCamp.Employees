0bac526fe131d26ff144fa1188a4c8a0be609c21
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-11-17 16:44:27 +0200

    Add project files.

---
+++ InCamp.Employees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InCamp.Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
